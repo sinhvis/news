09b0dc0242436f93c9c6b635ba36e2fb3761b4db
sinhvis <sinhvis@gmail.com>
2016-01-29 18:48:50 -0800

    Added new state for posts

--- app.js
+++ app.js
@@ -3,14 +3,20 @@ var app = angular.module('news', ['ui.router']);
 app.config([
     '$stateProvider',
     '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider) {
+    function ($stateProvider, $urlRouterProvider) {
 
         $stateProvider
         .state('home', {
             url: '/home',
             templateUrl: '/home.html',
             controller: 'MainCtrl'
-        });
+        }) ;
+
+        .state('posts', {
+            url: '/posts/{id}',
+            templateUrl: '/posts.html',
+            controller: 'PostsCtrl'
+        }) ;
 
         $urlRouterProvider.otherwise('home');
     }]) ;
