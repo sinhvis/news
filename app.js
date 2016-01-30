40433795b669b18dc53b500d3f644dec3d43a8fa
sinhvis <sinhvis@gmail.com>
2016-01-29 19:16:44 -0800

    Added comment

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.config([
             url: '/home',
             templateUrl: '/home.html',
             controller: 'MainCtrl'
-        }) ;
+        }) 
 
         .state('posts', {
             url: '/posts/{id}',
@@ -24,9 +24,9 @@ app.config([
 app.factory('posts', [function(){
     var o = {
         posts: []
-    };
+    } ;
     return o;
-}]);
+}]) ;
 
 app.controller('MainCtrl', [
     '$scope',
@@ -34,16 +34,29 @@ app.controller('MainCtrl', [
     function($scope, posts){
         $scope.posts = posts.posts;
         $scope.addPost = function(){
-            if(!$scope.title || $scope.title === '') { return; }
+            if(!$scope.title || $scope.title === '') { return ; }
             $scope.posts.push({
                 title: $scope.title,
                 link: $scope.link,
-                upvotes: 0
-            });
+                upvotes: 0,
+                comments: [
+                {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+                ]
+            }) ;
             $scope.title = '';
             $scope.link = '';
         }
+
         $scope.incrementUpvotes = function(post){
-            post.upvotes += 1;
+            post.upvotes += 1 ;
         }
     }]);
+
+app.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function ($scope, $stateParams, posts) {
+        $scope.post = posts.posts[$stateParams.id] ;
+    }]) ;
