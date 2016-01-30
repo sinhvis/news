1d25159c26ec687b1f0c5b45adcaba4f6ca9dbdc
sinhvis <sinhvis@gmail.com>
2016-01-29 19:33:14 -0800

    Add comments

--- app.js
+++ app.js
@@ -59,4 +59,14 @@ app.controller('PostsCtrl', [
     'posts',
     function ($scope, $stateParams, posts) {
         $scope.post = posts.posts[$stateParams.id] ;
+
+        $scope.addComment = function() {
+            if ($scope.body === '') { return ;}
+
+            $scope.post.comments.push({
+                body:       $scope.body,
+                author:     'user',
+                upvotes:    0
+            }) ;
+        } ;
     }]) ;
