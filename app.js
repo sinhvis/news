eff1ad7deafedae21699fdf87c3dd219f35521d3
sinhvis <sinhvis@gmail.com>
2016-01-30 18:32:15 -0800

    Changed file structure for Node

--- app.js
+++ app.js
@@ -1,72 +1,60 @@
-var app = angular.module('news', ['ui.router']);
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-app.config([
-    '$stateProvider',
-    '$urlRouterProvider',
-    function ($stateProvider, $urlRouterProvider) {
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-        $stateProvider
-        .state('home', {
-            url: '/home',
-            templateUrl: '/home.html',
-            controller: 'MainCtrl'
-        }) 
+var app = express();
 
-        .state('posts', {
-            url: '/posts/{id}',
-            templateUrl: '/posts.html',
-            controller: 'PostsCtrl'
-        }) ;
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-        $urlRouterProvider.otherwise('home');
-    }]) ;
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.factory('posts', [function(){
-    var o = {
-        posts: []
-    } ;
-    return o;
-}]) ;
+app.use('/', routes);
+app.use('/users', users);
 
-app.controller('MainCtrl', [
-    '$scope',
-    'posts',
-    function($scope, posts){
-        $scope.posts = posts.posts;
-        $scope.addPost = function(){
-            if(!$scope.title || $scope.title === '') { return ; }
-            $scope.posts.push({
-                title: $scope.title,
-                link: $scope.link,
-                upvotes: 0,
-                comments: [
-                {author: 'Joe', body: 'Cool post!', upvotes: 0},
-                {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-                ]
-            }) ;
-            $scope.title = '';
-            $scope.link = '';
-        }
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-        $scope.incrementUpvotes = function(post){
-            post.upvotes += 1 ;
-        }
-    }]);
+// error handlers
 
-app.controller('PostsCtrl', [
-    '$scope',
-    '$stateParams',
-    'posts',
-    function ($scope, $stateParams, posts) {
-        $scope.post = posts.posts[$stateParams.id] ;
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
-        $scope.addComment = function() {
-            if ($scope.body === '') { return ;}
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
-            $scope.post.comments.push({
-                body:       $scope.body,
-                author:     'user',
-                upvotes:    0
-            }) ;
-        } ;
-    }]) ;
+
+module.exports = app;
