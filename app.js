adbed837a001761dae1199360aa622232dab3221
sinhvis <sinhvis@gmail.com>
2016-02-01 13:42:24 -0800

    Added models, connected to server.js

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ var users = require('./routes/users');
 
 var app = express();
 
+var mongoose = require('mongoose') ;
+require('./models/Posts') ;
+mongoose.connect('mongodb://localhost/news') ;
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
