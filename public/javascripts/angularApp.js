41441f7fb58941f828aeb3765a03d6f5bd2b1f9c
sinhvis <sinhvis@gmail.com>
2016-02-03 16:55:09 -0800

    persistent data: create added to factory, and MainCtrl modified for adding posts

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -44,6 +44,13 @@ app.factory('posts', ['$http', function($http){
             angular.copy(data, o.posts) ;
         }) ;
     } ;
+
+    // create new posts
+    o.create = function(post) {
+        return $http.post('/posts', post).success(function(data) {
+            o.posts.push(data) ;
+        }) ;
+    } ;
     return o;
 }]) ;
 
@@ -54,23 +61,31 @@ app.controller('MainCtrl', [
         $scope.posts = posts.posts;
         $scope.addPost = function(){
             if(!$scope.title || $scope.title === '') { return ; }
-            $scope.posts.push({
+
+            // save posts to the server
+            posts.create({
                 title: $scope.title,
                 link: $scope.link,
-                upvotes: 0,
-                comments: [
-                {author: 'Joe', body: 'Cool post!', upvotes: 0},
-                {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-                ]
             }) ;
-            $scope.title = '';
-            $scope.link = '';
-        }
 
-        $scope.incrementUpvotes = function(post){
-            post.upvotes += 1 ;
-        }
-    }]);
+            // $scope.posts.push({
+            //     title: $scope.title,
+            //     link: $scope.link,
+            //     upvotes: 0,
+            //     comments: [
+            //     {author: 'Joe', body: 'Cool post!', upvotes: 0},
+            //     {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+            //     ]
+            // }) ;
+
+$scope.title = '';
+$scope.link = '';
+}
+
+$scope.incrementUpvotes = function(post){
+    post.upvotes += 1 ;
+}
+}]);
 
 app.controller('PostsCtrl', [
     '$scope',
