3e163040e1285fbe77ed3782b76533c202bd5486
sinhvis <sinhvis@gmail.com>
2016-02-04 20:21:24 -0800

    method generateJWT() to help generate tokens

--- models/Users.js
+++ models/Users.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose') ;
 var crypto = require('crypto') ;
+var jwt = require('jsonwebtoken') ;
 
 var UserSchema = new mongoose.Schema({
 	username: { type: String, lowercase: true, unique: true },
@@ -16,6 +17,20 @@ UserSchema.methods.validPassword = function(password) {
 	var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex') ;
 
 	return this.hash === hash ;
+} ;
+
+UserSchema.methods.generateJWT = function() {
+
+	// set expiration to 60 days
+	var today = Date() ;
+	var exp = new Date(today) ;
+	exp.setDate(today.getData() + 60) ;
+
+	return jwt.sign({
+		_id: 		this._id,
+		username: 	this.username,
+		exp: 		parseInt(exp.getTime() / 1000),
+	}, 'SECRET') ;
 } ;
 
 mongoose.model('User', UserSchema) ;
